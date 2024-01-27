432aab53116861034b9287ea650e952c0f5a7201
Erfan Jamali <108428634+jerfan367@users.noreply.github.com>
2024-01-27 19:25:59 +0330

    aha

--- lib/sub_pages/notifications_page.dart
+++ lib/sub_pages/notifications_page.dart
@@ -24,7 +24,7 @@ Widget thereIsMessageOrNot() {
   if (notificationsList.isEmpty) {
     return Column(
       children: [
-        SizedBox(height: 50),
+        const SizedBox(height: 50),
         Text(
           "There is nothing!",
           style: ts(Colors.deepOrange, 35, true),
@@ -32,24 +32,12 @@ Widget thereIsMessageOrNot() {
         const Spacer(),
         ClipRRect(
           borderRadius: BorderRadius.circular(10),
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://media1.tenor.com/m/DXu_d-6Pz4AAAAAC/napoleon-there-is-nothing-we-can-do.gif"),
-              ),
-            ),
-            child: Container(
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black54, blurRadius: 10, spreadRadius: 1),
-                ],
-              ),
-            ),
+          child: const Image(
+            image: NetworkImage(
+                "https://media1.tenor.com/m/DXu_d-6Pz4AAAAAC/napoleon-there-is-nothing-we-can-do.gif"),
           ),
         ),
-        SizedBox(height: 50),
+        const SizedBox(height: 50),
       ],
     );
   } else {
